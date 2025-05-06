f49933c3e341f5278b7672bcff058d6064dbed4b
Eric_Feng <2504170651@qq.com>
2025-05-07 01:03:09 +0800

    Add files via upload

--- cpu_1/cpu_1.srcs/sources_1/ip/instruction_mem/instruction_mem_stub.v
+++ cpu_1/cpu_1.srcs/sources_1/ip/instruction_mem/instruction_mem_stub.v
@@ -1,10 +1,10 @@
 // Copyright 1986-2017 Xilinx, Inc. All Rights Reserved.
 // --------------------------------------------------------------------------------
 // Tool Version: Vivado v.2017.4 (win64) Build 2086221 Fri Dec 15 20:55:39 MST 2017
-// Date        : Mon Apr 28 20:42:56 2025
+// Date        : Tue May  6 23:31:14 2025
 // Host        : Lekge2025 running 64-bit major release  (build 9200)
 // Command     : write_verilog -force -mode synth_stub
-//               e:/Sustech/25Spring_ComputerDesign/Project/cpu_1/cpu_1.srcs/sources_1/ip/instruction_mem/instruction_mem_stub.v
+//               E:/Sustech/25Spring_ComputerDesign/Project/cpu_1/cpu_1.srcs/sources_1/ip/instruction_mem/instruction_mem_stub.v
 // Design      : instruction_mem
 // Purpose     : Stub declaration of top-level module interface
 // Device      : xc7a35tcsg324-1
@@ -14,9 +14,11 @@
 // The synthesis directives are for Synopsys Synplify support to prevent IO buffer insertion.
 // Please paste the declaration into a Verilog source file or add the file as an additional source.
 (* x_core_info = "blk_mem_gen_v8_4_1,Vivado 2017.4" *)
-module instruction_mem(clka, addra, douta)
-/* synthesis syn_black_box black_box_pad_pin="clka,addra[13:0],douta[31:0]" */;
+module instruction_mem(clka, wea, addra, dina, douta)
+/* synthesis syn_black_box black_box_pad_pin="clka,wea[0:0],addra[13:0],dina[31:0],douta[31:0]" */;
   input clka;
+  input [0:0]wea;
   input [13:0]addra;
+  input [31:0]dina;
   output [31:0]douta;
 endmodule
