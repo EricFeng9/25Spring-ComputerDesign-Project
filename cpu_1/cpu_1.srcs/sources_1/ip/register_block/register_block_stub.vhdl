fcfd6b9946a95996b4af36d7bd6d5822570ff8b2
Shelia J. <12310645@mail.sustech.edu.cn>
2025-05-06 16:24:46 +0800

    Add files via upload

---
+++ cpu_1/cpu_1.srcs/sources_1/ip/register_block/register_block_stub.vhdl
@@ -0,0 +1,39 @@
+-- Copyright 1986-2017 Xilinx, Inc. All Rights Reserved.
+-- --------------------------------------------------------------------------------
+-- Tool Version: Vivado v.2017.4 (win64) Build 2086221 Fri Dec 15 20:55:39 MST 2017
+-- Date        : Sat May  3 14:58:40 2025
+-- Host        : tapakya1069 running 64-bit major release  (build 9200)
+-- Command     : write_vhdl -force -mode synth_stub
+--               c:/Users/BenjaminSo/Downloads/25Spring-ComputerDesign-Project-5.1/25Spring-ComputerDesign-Project-5.1/cpu_1/cpu_1.srcs/sources_1/ip/register_block/register_block_stub.vhdl
+-- Design      : register_block
+-- Purpose     : Stub declaration of top-level module interface
+-- Device      : xc7a35tcsg324-1
+-- --------------------------------------------------------------------------------
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+
+entity register_block is
+  Port ( 
+    clka : in STD_LOGIC;
+    wea : in STD_LOGIC_VECTOR ( 0 to 0 );
+    addra : in STD_LOGIC_VECTOR ( 4 downto 0 );
+    dina : in STD_LOGIC_VECTOR ( 31 downto 0 );
+    douta : out STD_LOGIC_VECTOR ( 31 downto 0 );
+    clkb : in STD_LOGIC;
+    web : in STD_LOGIC_VECTOR ( 0 to 0 );
+    addrb : in STD_LOGIC_VECTOR ( 4 downto 0 );
+    dinb : in STD_LOGIC_VECTOR ( 31 downto 0 );
+    doutb : out STD_LOGIC_VECTOR ( 31 downto 0 )
+  );
+
+end register_block;
+
+architecture stub of register_block is
+attribute syn_black_box : boolean;
+attribute black_box_pad_pin : string;
+attribute syn_black_box of stub : architecture is true;
+attribute black_box_pad_pin of stub : architecture is "clka,wea[0:0],addra[4:0],dina[31:0],douta[31:0],clkb,web[0:0],addrb[4:0],dinb[31:0],doutb[31:0]";
+attribute x_core_info : string;
+attribute x_core_info of stub : architecture is "blk_mem_gen_v8_4_1,Vivado 2017.4";
+begin
+end;
